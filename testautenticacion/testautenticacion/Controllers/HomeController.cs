ee0d38ae11278bcc8465916ce1b11c05e992cdc0
LuiFernanda <lfercaniz@gmail.com>
2022-10-12 23:42:12 -0600

    Prueba_Tecnica

---
+++ testautenticacion/testautenticacion/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace testautenticacion.Controllers
+{
+   // [Authorize]
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.idrol = Session["IdRol"];
+            return View();
+        }
+        
+        public ActionResult About()
+        {
+            ViewBag.Message = "Bienvenido a la pagina about.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Bienvenido a la pagina contacto.";
+
+            return View();
+        }
+        public ActionResult CerrarSesion()
+        {
+            FormsAuthentication.SignOut();
+            Session["Usuario"] = null;
+            return RedirectToAction("Index", "Acceso");
+        }
+    }
+}
---
+++ testautenticacion/testautenticacion/Models/Usuarios.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace testautenticacion.Models
+{
+    public class Usuarios
+    {
+        public String Nombres { get; set; }
+        public String Correo { get; set; }
+        public String Clave { get; set; }
+
+        public string IdRol { get; set; }
+
+    }
+}
